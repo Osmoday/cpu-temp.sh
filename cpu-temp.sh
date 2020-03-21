238bae81a05c3c91bc4309a20509170ef03fd669
Osmoday <45714482+Osmoday@users.noreply.github.com>
2020-03-21 15:17:28 +0100

    Update cpu-temp.sh

--- cpu-temp.sh
+++ cpu-temp.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-declare -a tempQueue=()
-declare -i queueIndex=0
+declare -a temp_queue=()
+declare -i queue_index=0
 declare -i OPERATIONS_PER_CYCLE=0
 declare -i TIME_FRAME=15
 declare -i CUTOFF=3150
 declare -i UPDATE_PERIOD=2
+declare -i highest_temp=0
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -42,21 +43,28 @@ CUTOFF=$(($TIME_FRAME*$NUMERATOR_READS_PER_MINUTE/$UPDATE_PERIOD))
 
 while true 
 do
-    if [ $queueIndex -ge $CUTOFF ]; then
-        queueIndex=0
+    if [ $queue_index -ge $CUTOFF ]; then
+        queue_index=0
+        highest_temp=0
     fi
-#     echo "${#tempQueue[@]}"
+#     echo "${#temp_queue[@]}"
     for f in /sys/class/hwmon/hwmon2/temp*_input ; do
-        tempQueue[$queueIndex]="$(cat $f)"
-#         echo "Check: "${tempQueue[$queueIndex]}
-        queueIndex=$queueIndex+1
+        temp_queue[$queue_index]="$(cat $f)"
+#         echo "Check: "${temp_queue[$queue_index]}
+        if [[ ${temp_queue[$queue_index]} -ge $highest_temp ]]; then
+            highest_temp=${temp_queue[$queue_index]}
+        fi
+        queue_index=$queue_index+1
     done
-    declare -i sum=$(IFS=+; echo "$((${tempQueue[*]}))")
-    declare -i avgTemp=$sum/${#tempQueue[@]}
+    declare -i sum=$(IFS=+; echo "$((${temp_queue[*]}))")
+    declare -i avgTemp=$sum/${#temp_queue[@]}
     printf "\033c"
     clear && echo -en "\e[3J"
 #     echo "Time frame: "$(($CUTOFF*$UPDATE_PERIOD/$NUMERATOR_READS_PER_MINUTE))" minutes"
     echo "Time frame: "$TIME_FRAME" minutes"
     echo "Average cpu temperature: "${avgTemp:0:2}"."${avgTemp:2:3}"°C"
+    echo "Highest cpu temperature: "${highest_temp:0:2}"."${highest_temp:2:3}"°C"
     sleep $UPDATE_PERIOD
 done
+
+#TODO: Switch to using system time instead of counting ops and sleeps (and implement a real queue), only remove highest temps which fall outside the rolling time windows instead of wiping them all every 20 minutes
